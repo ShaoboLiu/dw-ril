cf0d3a467c3b0539d7dcde240176e3b391ac58d5
liushaobo@yahoo.com <developer@action-android.(none)>
2017-03-28 08:29:42 -0400

    update preadb.sh

--- preadb.sh
+++ preadb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 scp developer@192.168.0.252:/home/developer/WorkspaceX/android/hardware/dw-ril/libreference-ril.so ./
+#scp developer@action-android:/home/developer/WorkspaceX/android/hardware/dw-ril/libreference-ril.so ./
 
 
 
