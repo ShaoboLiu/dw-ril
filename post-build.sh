a417dab6a2e6eaa40f93e120243937cfd77ed6a6
Shaobo <shaobo.liu@datawindcorp.com>
2017-03-28 11:32:01 -0400

    update post-build.sh

--- post-build.sh
+++ post-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp /home/developer/action_compiling_21/android/out/target/product/gs702c/obj/lib/libreference-ril.so ./libreference-ril.so
+
 # cp /home/developer/action_compiling_21/android/out/target/product/gs702c/system/lib/libreference-ril.so ./libreference-ril.so
 # cp /home/developer/action_compiling_21/leopard/build/gs702c/rootfs/android/lib/libreference-ril.so ./libreference-ril.so
 
