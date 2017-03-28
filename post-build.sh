1639c9c3644480e5074e6c5f480e0c253ae5d528
developer <developer@action-android.(none)>
2017-03-28 07:14:58 -0400

    update post-build.sh

---
+++ post-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cp /home/developer/action_compiling_21/android/out/target/product/gs702c/obj/lib/libreference-ril.so ./libdatawind-ril.so
+# cp /home/developer/action_compiling_21/android/out/target/product/gs702c/system/lib/libreference-ril.so ./libdatawind-ril.so
+# cp /home/developer/action_compiling_21/leopard/build/gs702c/rootfs/android/lib/libreference-ril.so ./libdatawind-ril.so
+
+
+
+
+
+
