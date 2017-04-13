791420e0c47082b6582d08d2871f1f6685eaf5b5
liushaobo@yahoo.com <developer@action-android.(none)>
2017-04-13 07:55:22 -0400

    upload adb-update.sh

---
+++ adb-update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+scp developer@192.168.0.252:/home/developer/WorkspaceX/android/hardware/dw-ril/libreference-ril.so ./
+#scp developer@action-android:/home/developer/WorkspaceX/android/hardware/dw-ril/libreference-ril.so ./
+
+
+
+
+
+
