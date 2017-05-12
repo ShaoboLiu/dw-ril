afcfb4a1d3b7670c518a38c359ea4382e82cebb7
ShaoboLiu <liushaobo@yahoo.com>
2017-05-12 16:18:49 -0400

    Delete prepare-build.sh

--- prepare-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cp ./4.4.2/reference-ril/reference-ril.c /home/developer/action_compiling_21/android/hardware/ril/reference-ril/reference-ril.c
-# cp ./4.4.2/reference-ril/atchannel.c /home/developer/action_compiling_21/android/hardware/ril/reference-ril/atchannel.c
-
-rm ./libreference-ril.so
-rm /home/developer/action_compiling_21/android/out/target/product/gs702c/obj/lib/libreference-ril.so
-rm /home/developer/action_compiling_21/android/out/target/product/gs702c/system/lib/libreference-ril.so
-
